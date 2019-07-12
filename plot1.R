4704110dda394e390018106746401e4671bc3b84
Pranjal Jain <46138278+pranjaljain99@users.noreply.github.com>
2019-07-13 00:51:20 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,13 @@
+> setwd("C:/Users/jpran/Downloads/Data Science Specializtion JHU")
 
+> housedata <-read.table("household_power_consumption.txt", header=TRUE, sep=";")
+
+> twodaydata <-rbind(housedata[housedata$Date=="1/2/2007",],housedata[housedata$Date=="2/2/2007",])
+
+> twodaydata$Date <- as.Date(twodaydata$Date,"%d/%m/%Y")
+
+> hist(as.numeric(twodaydata$Global_active_power), col="Red", main="Global Active Power", xlab="Global Active power (kilowatts)", ylab="Frequency")
+
+> png(file="C:/Users/jpran/Downloads/Data Science Specializtion JHU/plott.png",width=480, height=480)
+ 
+> dev.off()
