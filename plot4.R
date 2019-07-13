6e18680b03402e334b2f11965729f5ca17b597af
Pranjal Jain <46138278+pranjaljain99@users.noreply.github.com>
2019-07-13 15:26:19 +0100

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,30 @@
+> setwd("C:/Users/jpran/Downloads/Data Science Specializtion JHU")
+> housedata <-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+> twodaydata <-rbind(housedata[housedata$Date=="1/2/2007",],housedata[housedata$Date=="2/2/2007",])
+> datetime <- strptime(paste(twodaydata$Date, twodaydata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+> par(mfrow=c(2,2))
+> 
+> plot(datetime, d, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+> p<-as.numeric(as.character(twodaydata$Voltage))
+> 
+> plot(datetime, p, type="l", xlab="datetime", ylab="Voltage")
+> 
+> plot(datetime, twodaydata$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering",col="green",ylim=c(0,40))
+> 
+> par(new=TRUE)
+> 
+> par(new=TRUE)
+> plot(datetime, twodaydata$Sub_metering_2, type="l", xlab="", ylab="",col="red",ylim=c(0,40))
+> 
+> par(new=TRUE)
+> 
+> plot(datetime, twodaydata$Sub_metering_3, type="l", xlab="", ylab="",col="blue",ylim=c(0,40))
+> 
+> legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("green","red","blue"),cex=0.5)
+> 
+> q<-as.numeric(as.character(b$Global_reactive_power))
+Error: object 'b' not found
+> 
+> q<-as.numeric(as.character(twodaydata$Global_reactive_power))
+> 
+> plot(datetime, q, type="l", xlab="datetime", ylab="Global_reactive_power")
