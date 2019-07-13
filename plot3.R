8b846f11ef42f82a76c006ce08a25b988532a07b
Pranjal Jain <46138278+pranjaljain99@users.noreply.github.com>
2019-07-13 15:02:24 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,1 +1,11 @@
-
+> setwd("C:/Users/jpran/Downloads/Data Science Specializtion JHU")
+> housedata <-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+> twodaydata <-rbind(housedata[housedata$Date=="1/2/2007",],housedata[housedata$Date=="2/2/2007",])
+> datetime <- strptime(paste(twodaydata$Date, twodaydata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+> plot(datetime, twodaydata$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering",col="green",ylim=c(0,40))
+> par(new=TRUE)
+> plot(datetime, twodaydata$Sub_metering_2, type="l", xlab="", ylab="",col="red",ylim=c(0,40))
+> par(new=TRUE)
+> plot(datetime, twodaydata$Sub_metering_3, type="l", xlab="", ylab="",col="blue",ylim=c(0,40))
+> legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("green","red","blue"))
+> png(file="C:/Users/jpran/Downloads/Data Science Specializtion JHU/plot3.png",width=480, height=480)
